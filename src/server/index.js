273089361216fcc61bf2dbcfa0c97f43b48db35c
Derek Miranda <derek.miranda.1992@gmail.com>
2017-06-06 05:55:59 -0700

    consolidated content and data servers

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,13 @@
 // Front end server
 const express = require('express');
-const bodyParser = require('body-parser');
+const dataRouter = require('./routers/dataRouter');
 
 const PORT = 3000;
 const app = express();
 
-app.use(express.static(__dirname +'./../')); //serves the index.html
+app.use('/', dataRouter);
+
+app.use(express.static(__dirname +'/public'));
+app.use(express.static(__dirname +'/build'));
 
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
